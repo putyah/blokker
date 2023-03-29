2e29e1de59c52a8bb132936990d2160661920ec2
putyah <57195873+putyah@users.noreply.github.com>
2023-03-29 17:06:58 +0200

    Update scraper_blokker.py

--- scraper_blokker.py
+++ scraper_blokker.py
@@ -3,8 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import csv
+from datetime import date
+import os
 
-# # #Download sitemaps
+today = date.today()
+
+# #Download sitemaps
 print('Scrape started')
 print('Downloading base sitemap')
 sitemap_base = 'https://www.blokker.nl/sitemap_index.xml'
@@ -31,18 +35,16 @@ for link in product_xml:
     print('Done ' + link)
 
 #Filter product listing
-# filter = ['mepal', 'broodtrommel', 'amuse', 'vershouddoos', 'curver', 'brabantia', 'sistema', 'sareva', 'vershoudbak', 'isoleerfles', 'thermosfles', 'lunchbox', 'voorraadbus', 'sunware', 'berghoff', 'gefu', 'joseph-joseph', 'koziol', 'sigg', 'rotho', '-thermos-', 'point-virgule']
-# exclude_filter = ['emmer', 'vuilniszak', 'prullenbak', 'opbergmand','droogmolen', 'strijkplank', 'opbergbox','wasbox', 'wasmand', 'toiletborstel', 'serveerschaal', 'dinerbord']
-
-
-filter = ['thermosfles']
-exclude_filter = ['emmer', 'vuilniszak', 'prullenbak', 'opbergmand','droogmolen', 'strijkplank', 'opbergbox','wasbox', 'wasmand', 'toiletborstel', 'serveerschaal', 'dinerbord']
+filter = ['mepal', 'rosti']
+exclude_filter = ['lamp']
 
 df =  pd.read_csv('data/product_urls.csv')
+print(df.info())
 df =  df.drop_duplicates()
+print(df.info())
 df_clean = df[df['loc'].str.contains('|'.join(filter))]
 df_clean = df_clean[~df_clean['loc'].str.contains('|'.join(exclude_filter))]
-
+print(df.info())
 
 # Remover column header and index
 df_clean.to_csv('data/products_clean.csv',index=False)
@@ -53,8 +55,10 @@ print(df_clean.info())
 df = pd.read_csv('data/products_clean.csv')
 links = df['loc'].tolist()
 
-product_results = csv.writer(open(f'data/results_blokker.csv', 'a'))
-product_results.writerow(['EAN', 'Link', 'Brand', 'Price', 'Stock' ,'Category'])
+product_results = csv.writer(open(f'data/results_blokker_mepal_products.csv', 'a'))
+product_results.writerow(['Date', 'EAN', 'Link', 'Brand', 'Price', 'Stock' ,'Category', 'Seller', 'offerType'])
+
+# links = ['https://www.blokker.nl/mepal-beschuitdoos-stora---wit/1109480.html', 'https://www.blokker.nl/mepal-vleeswarendoos-modula-550%2F3---wit/1182000.html', 'https://www.blokker.nl/mepal-broodtrommel-campus-1-4-liter-lime/1827258.html']
 
 for link in links:
     response = requests.get(link)
@@ -72,4 +76,10 @@ for link in links:
     print(brand)
     category = jsonData['categoryChuck']
     print(category)
-    product_results.writerow([EAN, link, brand, price, stock, category])
+    offer = jsonData['offerType']
+    print(offer)
+    seller_data = soup.find(class_='seller__name')
+    seller_data = (seller_data.text.strip() if seller_data else "Geen waarde")
+    product_results.writerow([today, EAN, link, brand, price, stock, category, seller_data, offer])
+
+print('Scrape done!!')
